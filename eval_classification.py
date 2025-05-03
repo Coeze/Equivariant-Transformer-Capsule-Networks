ade8fa606b9d825605ef248a792b083f2376919c
Coeze <71006291+Coeze@users.noreply.github.com>
2025-05-03 12:21:32 +0100

    Update eval_classification.py

--- eval_classification.py
+++ eval_classification.py
@@ -7,10 +7,10 @@ import argparse
 import scipy.stats as stats
 import matplotlib.pyplot as plt
 from src.datasets import get_test_loader, DATASET_CONFIGS
-import src.models2 as m
+import src.models as m
 import numpy as np
 from src.train import test
-import neptune
+# import neptune
 import random
 import torch
 
@@ -27,7 +27,7 @@ parser.add_argument(
     "--dataset",
     type=str,
     required=True,
-    choices=["cifar10", "svhn", "smallnorb", "3diebench"],
+    choices=["cifar10", "svhn", "smallnorb"],
     help="Dataset name",
 )
 
@@ -47,12 +47,11 @@ parser.add_argument(
 parser.add_argument(
     "--wd", type=float, default=1e-6, help="Weight decay"
 )
-parser.add_argument(
-    "--save_dir", type=str, default="models/", help="Directory to save models"
-)
-parser.add_argument("--test", action="store_true", help="Train the model")
 
 # Model Architecture Settings
+parser.add_argument(
+    "--model_name", type=str, default='etcaps', help="name of model", choices=['etcaps', 'et', 'srcaps', 'resnet20']
+)
 parser.add_argument(
     "--encoder", type=str, default="resnet20", help="The resnet encoder to use"
 )
@@ -65,22 +64,6 @@ parser.add_argument(
 parser.add_argument(
     "--depth", type=int, default=1, help="Depth of the model"
 )
-parser.add_argument("--et", action="store_true", help="Set this flag to add a sequence of equivariant transformers before the backbone")
-
-# Training Environment Settings
-parser.add_argument("--num_workers", type=int, default=4, help="Number of worker threads for data loading")
-parser.add_argument(
-    "--world_size", type=int, default=None, 
-    help="Number of GPUs to use for distributed training. If not specified, all available GPUs will be used."
-)
-parser.add_argument("--port", type=int, default=52472)
-parser.add_argument(
-    "--exp_dir", type=Path, default=Path("experiments/default"), help="Experiment directory"
-)
-parser.add_argument(
-    "--root_log_dir", type=Path, default=Path("logs"), help="Root directory for logs"
-)
-
 parser.add_argument(
  "--path_to_model", type=Path, help="path to model"
 
@@ -93,34 +76,39 @@ def set_seed(seed):
     random.seed(seed)    
     
 def main():
-    
     args = parser.parse_args()
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+    checkpoint = torch.load(args.path_to_model, map_location=device)    
+    
+    if args.model_name == 'resnet20':
+        model = m.ResNet(args).to(device)
+        model.load_state_dict(checkpoint['model_state_dict'])
+    elif args.model_name == 'srcaps':
+        model = m.SRCAPS(args).to(device)
+        model.load_state_dict(checkpoint['model_state_dict'])
+    elif args.model_name == 'et':
+        model = m.Transformer(args).to(device)
+        model.load_state_dict(checkpoint['model_state_dict'])
+    elif args.model_name == 'etcaps':
+        model = m.ETCAPS(args).to(device)
+        model.load_state_dict(checkpoint['model_state_dict'])
+    else:
+        raise ValueError("Invalid model name. Choose from ['etcaps', 'et', 'srcaps', 'resnet20']")
+    
     test_loader = get_test_loader(data_dir=args.data_dir, dataset=args.dataset, batch_size=args.batch_size)
     errors = []
-    seeds = [42, 43, 44, 45, 46]
     for i in range(5):
-        set_seed(seeds[i])
-        error = test(args.path_to_model, test_loader, args)
+        set_seed(i+41)
+        error = test(model, test_loader)
         errors.append(error)
 
     errors = np.array(errors)
     mean_error = np.mean(errors)
-    std_error = np.std(errors)
-    confidence = 0.95
-    n = len(errors)
-    stderr = std_error / np.sqrt(n)
-    margin_of_error = stderr * stats.t.ppf((1 + confidence) / 2., n - 1)
 
-    print(mean_error, std_error)
+    print(f'Mean Error: {mean_error}')
 
-    plt.figure(figsize=(6, 4))
-    plt.bar(1, mean_error, yerr=margin_of_error, capsize=10, color='skyblue')
-    plt.xticks([1], ['Model'])
-    plt.ylabel('Classification Error')
-    plt.title('Classification Error with 95% Confidence Interval')
-    plt.grid(True, axis='y', linestyle='--', alpha=0.7)
-    plt.tight_layout()
-    plt.show()
     
 if __name__ == "__main__":
     main()
