0c5e063b55c9087142cdd4543116899cb38e0b09
Coeze <71006291+Coeze@users.noreply.github.com>
2025-05-04 20:45:14 +0100

    Update e2e_lee.py

--- liederiv/lee/e2e_lee.py
+++ liederiv/lee/e2e_lee.py
@@ -1,6 +1,7 @@
 import torch
 import pandas as pd
 from .lie_derivs import *
+import tqdm
 
 def get_equivariance_metrics(model, minibatch):
     x, y = minibatch
@@ -25,4 +26,17 @@ def get_equivariance_metrics(model, minibatch):
     
     metrics = {x: pd.Series(errs[x].abs().cpu().data.numpy().mean(-1)) for x in errs}
     df = pd.DataFrame.from_dict(metrics)
+    return df
+
+
+
+def eval_average_metrics_wstd(loader, metrics, max_mbs=None):
+    total = len(loader) if max_mbs is None else min(max_mbs, len(loader))
+    dfs = []
+    with torch.no_grad():
+        for idx, minibatch in tqdm.tqdm(enumerate(loader), total=total):
+            dfs.append(metrics(minibatch))
+            if max_mbs is not None and idx >= max_mbs:
+                break
+    df = pd.concat(dfs)
     return df
